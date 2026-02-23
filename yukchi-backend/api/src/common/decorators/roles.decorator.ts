11a8c858f6be85e8220d522b320bda19edf1985b
bobur5012 <botiroff.b@gmail.com>
2026-02-23 05:45:46 +0500

    Frontend only: exclude yukchi-backend from repo

--- yukchi-backend/api/src/common/decorators/roles.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { SetMetadata } from '@nestjs/common';
-
-export const ROLES_KEY = 'roles';
-export type Role = 'admin' | 'courier';
-
-export const Roles = (...roles: Role[]) => SetMetadata(ROLES_KEY, roles);
-export const Public = () => SetMetadata('isPublic', true);
--- yukchi-backend/api/src/modules/notification/queues/queue.constants.ts
+++
@@ -1,4 +0,0 @@
-export const BOT_QUEUE = 'BOT_QUEUE';
-export const CLIENT_QUEUE = 'CLIENT_QUEUE';
-export const BOT_QUEUE_NAME = 'notifications-bot';
-export const CLIENT_QUEUE_NAME = 'notifications-client';
